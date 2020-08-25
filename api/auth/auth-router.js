a2d969e0e66dcd1be729ab48e3e13706f07716ec
charlupuhh <62491397+charlupuhh@users.noreply.github.com>
2020-08-24 19:05:19 -0700

    returning auth user

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -19,7 +19,7 @@ router.post('/register', (req, res) => {
     })
     .catch(error => {
       console.log(error);
-      res.status(500).json(error);
+      res.status(500).json({error: error, errorMessage: error.message});
     })
 });
 
