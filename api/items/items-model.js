084508b18f1447ca2e2a6260beef36dbdc1d8d92
charlupuhh <62491397+charlupuhh@users.noreply.github.com>
2020-08-25 17:10:08 -0700

    item id not seller

--- api/items/items-model.js
+++ api/items/items-model.js
@@ -17,7 +17,7 @@ module.exports = {
   
   async function add(item) {
     try {
-      const [id] = await db("items").insert(item, "sellerID");
+      const [id] = await db("items").insert(item, "id");
       return findById(id);
     } catch (error) {
       throw error;
